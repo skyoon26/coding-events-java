1ebb166ec0570b55abef70a949bf95991db631b6
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-05-01 10:40:58 -0500

    Add new fragment file and update code

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -11,27 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("events/")
+@RequestMapping("events")
 public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
+    public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
-        return "redirect:";
+        return "redirect:/events";
     }
 }
