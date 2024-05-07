16c805f75eca43a30dc48478ac44796118cbda81
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-05-06 19:55:34 -0500

    Delete an event

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -33,4 +33,24 @@ public class EventController {
         EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
     }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.removeEvent(id);
+            }
+        }
+
+        return "redirect:/events";
+    }
+
 }
